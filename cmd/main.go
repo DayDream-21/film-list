4fcc7571590863563fb5ae1493f131682ab8a10c
Slava Mashkov <mashkov.vd@gmail.com>
2024-02-20 22:48:35 +0300

    Add log and server setup depending on current config

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"film-list/internal/config"
 	dbDelete "film-list/internal/http-server/handler/film/delete"
 	"film-list/internal/http-server/handler/film/get"
 	"film-list/internal/http-server/handler/film/save"
@@ -8,14 +9,15 @@ import (
 	"film-list/internal/storage/mongo"
 	"log/slog"
 	"net/http"
+	"os"
 )
 
-// TODO: выложить на github
 func main() {
-	log := charmLogger.New()
+	cfg := config.MustLoad()
+	log := setupLogger(cfg.Env)
 
-	// TODO: парсить конфигурацию из файла, на основе конфигурации выбирать тип логгирования и ту БД и к которой мы подключаемся.
-	//  Сделать два режима продакшен и разработки. В проде мы подключаемся к удаленной БД, а в разработке к локальной
+	// TODO: парсить конфигурацию из файла, на основе конфигурации выбирать ту БД и к которой мы подключаемся.
+	//  В проде мы подключаемся к удаленной БД, а в разработке к локальной
 	// TODO: добавить реализацию на PostgreSQL (может разбить на разные storage, типо sqlStorage и nosqlStorage?)
 	storage, err := mongo.New(log)
 	if err != nil {
@@ -26,8 +28,42 @@ func main() {
 	http.HandleFunc("POST /add-film", save.New(storage, log))
 	http.HandleFunc("DELETE /film", dbDelete.New(storage, log))
 
-	err = http.ListenAndServe(":8080", nil)
-	if err != nil {
+	server := &http.Server{
+		Addr:         cfg.Address,
+		Handler:      nil,
+		ReadTimeout:  cfg.HTTPServer.Timeout,
+		WriteTimeout: cfg.HTTPServer.Timeout,
+		IdleTimeout:  cfg.HTTPServer.IdleTimeout,
+	}
+
+	if err = server.ListenAndServe(); err != nil {
 		slog.Error("failed to start server:", err)
 	}
+}
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = charmLogger.New()
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		fallthrough
+	default:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
